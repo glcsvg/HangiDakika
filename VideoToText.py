a984f0785436e2a8d9b6e43fd237ac491ff6d500
Muhammed Ali <52376621+Muhammedali26@users.noreply.github.com>
2024-01-29 11:49:49 +0300

    Add files via upload
    YouTube Video Transkribe Scripti
    Bu Python scripti, YouTube videolarını indirme, videolardan ses çıkarma ve bu sesleri metne dönüştürme işlemlerini otomatikleştirmek için tasarlanmıştır. Projenin bu parçası, daha büyük bir uygulamanın modüler bir bileşeni olarak kullanılmak üzere hazırlanmıştır.
    
    Özellikler
    YouTube Video İndirme: Seçilen YouTube videolarını otomatik olarak indirebilme.
    Ses Çıkarma: İndirilen videolardan otomatik olarak ses dosyalarını çıkarabilme.
    Ses Dosyasını Transkribe Etme: Çıkarılan ses dosyalarını OpenAI'nin API'si kullanarak metne dönüştürebilme.
    
    Entegrasyon
    Bu script, Python 3 ile uyumludur ve pydub, moviepy, pytube, ve openai kütüphanelerini kullanır. Scripti, kendi projenize entegre etmek için aşağıdaki adımları izleyin:
    
    Gerekli Kütüphanelerin Kurulumu: Bu scripti çalıştırmadan önce, ilgili kütüphaneleri kurun:
    
    pip install pydub moviepy pytube openai
    
    API Anahtarının Ayarlanması: OpenAI'nin ses transkribe özelliğini kullanmak için, geçerli bir OpenAI API anahtarına ihtiyacınız vardır. Bu anahtar, ortam değişkeni olarak ayarlanmalıdır:
    
    export OPENAI_API_KEY='your-api-key'
    
    Scriptin Kullanımı: Scripti, projenize dahil edin ve gerekli parametrelerle çalıştırın.

---
+++ VideoToText.py
@@ -0,0 +1,48 @@
+from moviepy.editor import VideoFileClip
+from pytube import YouTube
+import openai
+import os
+
+# API Anahtarını Ortam Değişkeninden Al
+openai.api_key = os.getenv("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")
+
+
+def extract_audio(input_video_path, output_audio_path, bitrate="32k"):
+    """Video dosyasından ses dosyasını çıkarır."""
+    try:
+        video_clip = VideoFileClip(input_video_path)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(output_audio_path, bitrate=bitrate)
+    finally:
+        video_clip.close()
+
+
+def download_video(url, filename):
+    """YouTube'dan video indirir."""
+    yt = YouTube(url)
+    yt.streams.filter(file_extension="mp4").first().download(filename=filename)
+
+
+def transcribe_audio(file_path):
+    """Ses dosyasını transkribe eder."""
+    with open(file_path, "rb") as file:
+        transcription = openai.Audio.transcribe("whisper-1", file)
+        return transcription["text"].encode("utf-8").decode("utf-8")
+
+
+def main():
+    url = "https://www.youtube.com/watch?v=2gPiiHnXhZw"
+    filename = "gezeravci.mp4"
+    input_video_path = "c:\\Users\\muham\\Desktop\\AA_Hackathon\\gezeravci.mp4"
+    output_audio_path = "c:\\Users\\muham\\Desktop\\AA_Hackathon\\gezeravci.mp3"
+
+    extract_audio(input_video_path, output_audio_path)
+    download_video(url, filename=filename)
+    transcribed_text = transcribe_audio(output_audio_path)
+
+    with open("transcription.txt", "w", encoding="utf-8") as text_file:
+        text_file.write(transcribed_text)
+
+
+if __name__ == "__main__":
+    main()
